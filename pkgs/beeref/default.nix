d399c33d741495f5b443a9dd4447588906b3cdee
uniquepointer <uniquepointer@mailbox.org>
2021-10-29 00:25:13 -0500

    got some of it weorking

--- pkgs/beeref/default.nix
+++ pkgs/beeref/default.nix
@@ -1,33 +1,57 @@
-{ stdenv, autoPatchelfHook, fetchurl, lib }:
+{ lib, fetchFromGitHub, python3, python3Packages }:
 
-stdenv.mkDerivation rec {
-  pname = "beeref";
-  version = "0.2.0";
-
-  src = fetchurl {
-    url = "https://github.com/rbreu/beeref/releases/download/v${version}/BeeRef-${version}-linux";
-    sha256 = "sha256-jpjf07l4NkmAXgZyOLP03wDB2iSwErlS5EdXUQzyQdI=";
+let
+  rectangle-packer = python3.pkgs.buildPythonPackage rec {
+    pname = "rectangle-packer";
+    version = "2.0.1";
+    src = python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-3Y5wGE0q+KGV1WjgwsPMGX8MlMKkd0dUEeXql4xnsn0=";
+    };
+    doCheck = false;
+  nativeBuildInputs = [ python3Packages.cython ];
   };
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+in
+python3.pkgs.buildPythonApplication rec {
+  pname = "beeref";
+  version = "0.3.0-dev";
 
-  phases = ["installPhase" "patchPhase" "postFixup"];
+  src = fetchFromGitHub {
+    owner = "rbreu";
+    repo = "beeref";
+    rev = "cf55d1a5e304da707f2cc9a9ca793ee24503a0bc";
+    sha256 = "sha256-Qx1dV5yHK6SxCEh+vXfeeBR8qU7GdFoH9n4dNl4PVbw=";
+  };
 
-  installPhase = ''
-    cp $src beeref
-    install -m755 -D "beeref" "$out/bin/beeref"
+
+  propagatedBuildInputs = with python3.pkgs;
+    [
+      flake8
+      yamllint
+      coverage
+      httpretty
+      pytest
+      pytest-qt
+      pyqt5
+      pyqt6
+      markdown2
+      maestral
+      exif
+      rectangle-packer
+    ];
+  buildPhase = ''
+    python setup.py build
   '';
-
-  postFixup = ''
-    patchelf "$out/bin/beeref"
+  installPhase = ''
+    mkdir -p $out
+    python setup.py install --prefix="$out"
   '';
 
   meta = with lib; {
     description = "A Simple Reference Image Viewer.";
     homepage = "https://beeref.org/";
+    license = licenses.gpl3Only;
     maintainers = with maintainers; [ uniquepointer ];
     platforms = platforms.linux;
-    broken = true;
   };
 }
